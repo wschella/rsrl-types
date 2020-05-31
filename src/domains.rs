336d6acf8fe2ca4ca7f764a8c6c9295af323e719
Wout Schellaert <wout.schellaert@gmail.com>
2020-05-31 10:22:53 +0200

    Rustfmt

--- src/domains.rs
+++ src/domains.rs
@@ -7,182 +7,182 @@ pub type Action<D> = <<D as Domain>::ActionSpace as Space>::Value;
 
 /// An interface for constructing reinforcement learning problem domains.
 pub trait Domain {
-  /// State space representation type class.
-  type StateSpace: Space;
+    /// State space representation type class.
+    type StateSpace: Space;
 
-  /// Action space representation type class.
-  type ActionSpace: Space;
+    /// Action space representation type class.
+    type ActionSpace: Space;
 
-  /// Emit an observation of the current state of the environment.
-  fn emit(&self) -> Observation<State<Self>>;
+    /// Emit an observation of the current state of the environment.
+    fn emit(&self) -> Observation<State<Self>>;
 
-  /// Transition the environment forward a single step given an action, `a`.
-  fn step(&mut self, a: Action<Self>) -> Transition<State<Self>, Action<Self>>;
+    /// Transition the environment forward a single step given an action, `a`.
+    fn step(&mut self, a: Action<Self>) -> Transition<State<Self>, Action<Self>>;
 
-  fn rollout(
-    mut self,
-    actor: impl Fn(&State<Self>) -> Action<Self>,
-  ) -> Vec<Transition<State<Self>, Action<Self>>>
-  where
-    Self: Sized,
-  {
-    let first = Some(self.step(self.emit().map_into(&actor)));
+    fn rollout(
+        mut self,
+        actor: impl Fn(&State<Self>) -> Action<Self>,
+    ) -> Vec<Transition<State<Self>, Action<Self>>>
+    where
+        Self: Sized,
+    {
+        let first = Some(self.step(self.emit().map_into(&actor)));
 
-    iter::successors(first, |t| match t.to {
-      Observation::Terminal(_) => None,
-      Observation::Full(ref s) | Observation::Partial(ref s) => Some(self.step(actor(s))),
-    })
-    .collect()
-  }
+        iter::successors(first, |t| match t.to {
+            Observation::Terminal(_) => None,
+            Observation::Full(ref s) | Observation::Partial(ref s) => Some(self.step(actor(s))),
+        })
+        .collect()
+    }
 
-  /// Returns an instance of the state space type class.
-  fn state_space(&self) -> Self::StateSpace;
+    /// Returns an instance of the state space type class.
+    fn state_space(&self) -> Self::StateSpace;
 
-  /// Returns an instance of the action space type class.
-  fn action_space(&self) -> Self::ActionSpace;
+    /// Returns an instance of the action space type class.
+    fn action_space(&self) -> Self::ActionSpace;
 }
 
 /// Container class for data associated with a domain transition.
 #[derive(Clone, Copy, Debug)]
 pub struct Transition<S, A> {
-  /// State transitioned _from_, `s`.
-  pub from: Observation<S>,
+    /// State transitioned _from_, `s`.
+    pub from: Observation<S>,
 
-  /// Action taken to initiate the transition (control tasks).
-  pub action: A,
+    /// Action taken to initiate the transition (control tasks).
+    pub action: A,
 
-  /// Reward obtained from the transition.
-  pub reward: f64,
+    /// Reward obtained from the transition.
+    pub reward: f64,
 
-  /// State transitioned _to_, `s'`.
-  pub to: Observation<S>,
+    /// State transitioned _to_, `s'`.
+    pub to: Observation<S>,
 }
 
 impl<S, A> Transition<S, A> {
-  /// Return references to the `from` and `to` states associated with this
-  /// transition.
-  pub fn states(&self) -> (&S, &S) {
-    (self.from.state(), self.to.state())
-  }
-
-  /// Apply a closure to the `from` and `to` states associated with this
-  /// transition.
-  pub fn map_states<O>(self, f: impl Fn(&S) -> O) -> Transition<O, A> {
-    Transition {
-      from: self.from.map(&f),
-      action: self.action,
-      reward: self.reward,
-      to: self.to.map(f),
+    /// Return references to the `from` and `to` states associated with this
+    /// transition.
+    pub fn states(&self) -> (&S, &S) {
+        (self.from.state(), self.to.state())
     }
-  }
 
-  /// Returns true if the transition ends in a terminal state.
-  pub fn terminated(&self) -> bool {
-    self.to.is_terminal()
-  }
-
-  /// Replace the action associated with this transition and return a new
-  /// instance.
-  pub fn replace_action<T>(self, action: T) -> Transition<S, T> {
-    Transition {
-      from: self.from,
-      action: action,
-      reward: self.reward,
-      to: self.to,
+    /// Apply a closure to the `from` and `to` states associated with this
+    /// transition.
+    pub fn map_states<O>(self, f: impl Fn(&S) -> O) -> Transition<O, A> {
+        Transition {
+            from: self.from.map(&f),
+            action: self.action,
+            reward: self.reward,
+            to: self.to.map(f),
+        }
     }
-  }
 
-  /// Drop the action associated with this transition and return a new
-  /// instance.
-  pub fn drop_action(self) -> Transition<S, ()> {
-    self.replace_action(())
-  }
-
-  pub fn negate_reward(self) -> Transition<S, A> {
-    Transition {
-      from: self.from,
-      action: self.action,
-      reward: -self.reward,
-      to: self.to,
+    /// Returns true if the transition ends in a terminal state.
+    pub fn terminated(&self) -> bool {
+        self.to.is_terminal()
     }
-  }
+
+    /// Replace the action associated with this transition and return a new
+    /// instance.
+    pub fn replace_action<T>(self, action: T) -> Transition<S, T> {
+        Transition {
+            from: self.from,
+            action: action,
+            reward: self.reward,
+            to: self.to,
+        }
+    }
+
+    /// Drop the action associated with this transition and return a new
+    /// instance.
+    pub fn drop_action(self) -> Transition<S, ()> {
+        self.replace_action(())
+    }
+
+    pub fn negate_reward(self) -> Transition<S, A> {
+        Transition {
+            from: self.from,
+            action: self.action,
+            reward: -self.reward,
+            to: self.to,
+        }
+    }
 }
 
 /// Container class for data associated with a domain observation.
 #[derive(Clone, Copy, Debug)]
 pub enum Observation<S> {
-  /// Fully observed state of the environment.
-  Full(S),
+    /// Fully observed state of the environment.
+    Full(S),
 
-  /// Partially observed state of the environment.
-  Partial(S),
+    /// Partially observed state of the environment.
+    Partial(S),
 
-  /// Terminal state of the environment.
-  Terminal(S),
+    /// Terminal state of the environment.
+    Terminal(S),
 }
 
 impl<S> Observation<S> {
-  /// Helper function returning a reference to the state values for the given
-  /// observation.
-  pub fn state(&self) -> &S {
-    use self::Observation::*;
+    /// Helper function returning a reference to the state values for the given
+    /// observation.
+    pub fn state(&self) -> &S {
+        use self::Observation::*;
 
-    match self {
-      Full(ref state) | Partial(ref state) | Terminal(ref state) => state,
+        match self {
+            Full(ref state) | Partial(ref state) | Terminal(ref state) => state,
+        }
     }
-  }
 
-  pub fn map<O>(&self, f: impl Fn(&S) -> O) -> Observation<O> {
-    use self::Observation::*;
+    pub fn map<O>(&self, f: impl Fn(&S) -> O) -> Observation<O> {
+        use self::Observation::*;
 
-    match self {
-      Full(ref state) => Full(f(state)),
-      Partial(ref state) => Partial(f(state)),
-      Terminal(ref state) => Terminal(f(state)),
+        match self {
+            Full(ref state) => Full(f(state)),
+            Partial(ref state) => Partial(f(state)),
+            Terminal(ref state) => Terminal(f(state)),
+        }
     }
-  }
 
-  pub fn map_into<O>(&self, f: impl Fn(&S) -> O) -> O {
-    use self::Observation::*;
+    pub fn map_into<O>(&self, f: impl Fn(&S) -> O) -> O {
+        use self::Observation::*;
 
-    match self {
-      Full(ref state) | Partial(ref state) | Terminal(ref state) => f(state),
+        match self {
+            Full(ref state) | Partial(ref state) | Terminal(ref state) => f(state),
+        }
     }
-  }
 
-  /// Returns true if the state was fully observed, otherwise false.
-  pub fn is_full(&self) -> bool {
-    match self {
-      Observation::Full(_) => true,
-      _ => false,
+    /// Returns true if the state was fully observed, otherwise false.
+    pub fn is_full(&self) -> bool {
+        match self {
+            Observation::Full(_) => true,
+            _ => false,
+        }
     }
-  }
 
-  /// Returns true if the state was only partially observed, otherwise false.
-  pub fn is_partial(&self) -> bool {
-    match self {
-      Observation::Partial(_) => true,
-      _ => false,
+    /// Returns true if the state was only partially observed, otherwise false.
+    pub fn is_partial(&self) -> bool {
+        match self {
+            Observation::Partial(_) => true,
+            _ => false,
+        }
     }
-  }
 
-  /// Returns true if the observation is the terminal state, otherwise false.
-  pub fn is_terminal(&self) -> bool {
-    match self {
-      Observation::Terminal(_) => true,
-      _ => false,
+    /// Returns true if the observation is the terminal state, otherwise false.
+    pub fn is_terminal(&self) -> bool {
+        match self {
+            Observation::Terminal(_) => true,
+            _ => false,
+        }
     }
-  }
 }
 
 macro_rules! impl_into {
-  (Transition < S, $type:ty > => Transition < S,() >) => {
-    impl<S> Into<Transition<S, ()>> for Transition<S, $type> {
-      fn into(self) -> Transition<S, ()> {
-        self.drop_action()
-      }
-    }
-  };
+    (Transition < S, $type:ty > => Transition < S,() >) => {
+        impl<S> Into<Transition<S, ()>> for Transition<S, $type> {
+            fn into(self) -> Transition<S, ()> {
+                self.drop_action()
+            }
+        }
+    };
 }
 
 impl_into!(Transition<S, u8> => Transition<S, ()>);
